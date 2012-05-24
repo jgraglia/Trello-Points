556817d23f0b5d2e24d871e64fd0aa6c11f71aad
Julien Graglia <jgraglia@netceler.com>
2012-05-24 22:48:01 +0200

    v0.35

--- packageit.sh
+++ packageit.sh
@@ -6,7 +6,7 @@ ZIP=../trello-points.v$VERSION.zip
 echo "Press a key to continue (and build $ZIP) or CTRL-C"
 read aaa
 rm -f $ZIP || true
-zip -r $ZIP *
+zip -r $ZIP * -x tests/* -x screenshots/* -x tests/libs/*
 echo "Done."
 unzip -l $ZIP
 ls -alh $ZIP
