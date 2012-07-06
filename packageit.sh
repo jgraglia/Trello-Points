345b748b66c1aaa31b96e5cca9b1aa3332d2acb1
Julien Graglia <jgraglia@netceler.com>
2012-07-06 11:17:32 +0200

    handle v automatically

--- packageit.sh
+++ packageit.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-echo "Version: "
-read VERSION
-ZIP=../points-for-trello.v$VERSION.zip
+echo "Zips found: "
 ls -alh ../*.zip
+echo "Version found in manifest.json"
+fgrep \"version manifest.json
+
+VERSION=`fgrep \"version manifest.json | cut -d '"' -f 4`
+echo "Default version is (found in manifest.json): $VERSION"
+echo "Ctrl+C if sth wrong or..."
 echo "Press a key to continue (and build $ZIP) or CTRL-C"
 read aaa
+ZIP=../points-for-trello.v$VERSION.zip
 rm -f $ZIP || true
 zip -r $ZIP * -x tests/* -x screenshots/* -x tests/libs/*
 echo "Done."
