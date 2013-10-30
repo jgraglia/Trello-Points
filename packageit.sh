d3ae6155b85d920ac4e66dedf0c25a02e2d7c58f
Julien Graglia <jgraglia@netceler.com>
2013-10-30 08:43:34 +0100

    update release script

--- packageit.sh
+++ packageit.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+DISTFOLDER=dist
 echo "Zips found: "
-ls -alh ../*.zip
+ls -alh $DISTFOLDER/*.zip
 echo "Version found in manifest.json"
 fgrep \"version manifest.json
 
@@ -10,7 +11,7 @@ echo "Default version is (found in manifest.json): $VERSION"
 echo "Ctrl+C if sth wrong or..."
 echo "Press a key to continue (and build $ZIP) or CTRL-C"
 read aaa
-ZIP=../points-for-trello.v$VERSION.zip
+ZIP=$DISTFOLDER/points-for-trello.v$VERSION.zip
 rm -f $ZIP || true
 zip -r $ZIP * -x tests/* -x screenshots/* -x tests/libs/*
 echo "Done."
